4b31846bd4938b7e2a6838cdfd75f69de9f406c4
Dheeresh Agarwal <da11@iitbbs.ac.in>
2025-05-02 23:57:51 +0530

    icons add

--- src/components/Icon/IconCard.tsx
+++ src/components/Icon/IconCard.tsx
@@ -4,10 +4,24 @@ interface IconCardProps {
   icon: Icon;
 }
 
+const size = (icon: Icon) => {
+  const iconCategory = icon.category.toLowerCase();
+  switch (true) {
+    case iconCategory === "others":
+      return 3;
+    default:
+      return 2;
+  }
+};
+
 const IconCard: React.FC<IconCardProps> = ({ icon }) => {
   return (
     <div className="rounded-lg border-2 hover:border-blue-500 hover:cursor-pointer flex gap-3 p-3 sm:p-4 items-center w-full">
-      <i className={`ci ci-${icon.classes[0]} ci-2x sm:ci-3x`}></i>
+      <i
+        className={`ci ci-${icon.classes[0]} ci-${size(icon)}x sm:ci-${
+          size(icon) + 1
+        }x`}
+      ></i>
       <span className="flex flex-col gap-1">
         <h4 className="text-gray-800 font-semibold truncate text-sm">
           {icon.name}
