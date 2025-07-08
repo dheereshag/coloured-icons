d2503548e32feef09592aa841486c33033280a3a
Dheeresh Agarwal <da11@iitbbs.ac.in>
2025-07-08 13:25:16 +0530

    hero heading change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,21 +5,18 @@ import Link from "next/link";
 import { FaStar } from "react-icons/fa6";
 import { outfit } from "@/lib/fonts";
 
+const heroHeading = "mt-4 text-5xl sm:text-6xl text-gray-900 font-semibold";
+
 export default function Home() {
   return (
     <div className={`relative isolate ${outfit.className} mb-10`}>
-      <section className="mx-auto max-w-2xl py-8 sm:py-14 lg:py-20 text-center">
-        <h1
-          className={`mt-4 text-balance text-5xl sm:text-7xl ${outfit.className} tracking-tight text-gray-900 font-semibold`}
-        >
-          Beautiful Icons. <br />
-          Endless Possibilities
-        </h1>
-        <h2 className="mt-6 text-pretty text-lg font-medium text-gray-600 sm:text-xl/8 max-w-md mx-auto">
-          Your one-stop destination for stunning coloured icons. Find the
-          perfect icon to bring your designs to life.
+      <section className="mx-auto py-8 max-w-5xl sm:py-14 lg:py-20 text-center">
+        <h1 className={heroHeading}>Zero downloads. Zero build steps.</h1>
+        <h1 className={heroHeading}>Just pure CDN magic.</h1>
+        <h2 className="mt-6 text-pretty text-lg font-medium text-gray-600 sm:text-xl/8 max-w-lg mx-auto">
+          From React to MongoDB, Tesla to Spotify-every icon your project needs,
+          instantly accessible via CDN.
         </h2>
-
         <div className="mt-6 flex justify-center">
           <Link
             href="https://github.com/dheereshagrwal/coloured-icons"
