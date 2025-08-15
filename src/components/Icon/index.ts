b823e9007a74c2bcc2be7b159294439f63f12cf8
Dheeresh Agarwal <da11@iitbbs.ac.in>
2025-08-15 07:43:59 +0530

    refactor

--- src/components/Icon/index.ts
+++ src/components/Icon/index.ts
@@ -1,3 +1,3 @@
 export { default as IconCard } from "./IconCard";
-export { default as IconCode } from "./IconCode";
+export { default as IconCode } from "../Modal/IconCode";
 export { default as IconList } from "./IconList";
